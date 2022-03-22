17891af632abe1644a134576aaef8ff6a7fdfeb7
Sean <56064050+seangoats@users.noreply.github.com>
2022-03-22 17:28:54 +0800

    Fix frontend tests

--- frontend/__tests__/components/auth/SignInForm.spec.js
+++ frontend/__tests__/components/auth/SignInForm.spec.js
@@ -1,32 +1,52 @@
 import SignInForm from '@/components/auth/SignInForm';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '@/app/slices/authSlice';
+
+let mockStore;
 
 describe('auth/SignInForm', () => {
-	it('should render the form title,  fields, sign in button', () => {
-		render(<SignInForm />);
-		//title
-		expect(screen.getByText('Sign in to VolunteerGoWhere')).toBeInTheDocument();
-		//fields
-		const fields = ['Email', 'Password'];
-		fields.forEach(field => expect(screen.getByLabelText(field)).toBeInTheDocument());
-		//sign in button
-		expect(
-			screen.getByRole('button', {
-				name: /sign-in/i,
-			})
-		).toBeInTheDocument();
-	});
+    beforeEach(() => {
+        mockStore = configureStore({
+            reducer: {
+                auth: authReducer,
+            },
+        });
+    });
+    it('should render the form title,  fields, sign in button', () => {
+        render(
+            <Provider store={mockStore}>
+                <SignInForm />
+            </Provider>
+        );
+        //title
+        expect(screen.getByText('Sign in to VolunteerGoWhere')).toBeInTheDocument();
+        //fields
+        const fields = ['Email', 'Password'];
+        fields.forEach((field) => expect(screen.getByLabelText(field)).toBeInTheDocument());
+        //sign in button
+        expect(
+            screen.getByRole('button', {
+                name: /sign-in/i,
+            })
+        ).toBeInTheDocument();
+    });
 
-	it('should show the errors if fields not filled', async () => {
-		render(<SignInForm />);
-		await waitFor(() => {
-			fireEvent.click(
-				screen.getByRole('button', {
-					name: /sign-in/i,
-				})
-			);
-		});
-		const errorMessages = ['Email is required', 'Password is required']
-		errorMessages.forEach(message => expect(screen.getByText(message)).toBeInTheDocument())
-	});
+    it('should show the errors if fields not filled', async () => {
+        render(
+            <Provider store={mockStore}>
+                <SignInForm />
+            </Provider>
+        );
+        await waitFor(() => {
+            fireEvent.click(
+                screen.getByRole('button', {
+                    name: /sign-in/i,
+                })
+            );
+        });
+        const errorMessages = ['Email is required', 'Password is required'];
+        errorMessages.forEach((message) => expect(screen.getByText(message)).toBeInTheDocument());
+    });
 });
--- frontend/__tests__/components/auth/VolunteerSignUpForm.spec.js
+++ frontend/__tests__/components/auth/VolunteerSignUpForm.spec.js
@@ -1,9 +1,25 @@
 import VolunteerSignUpForm from '@/components/auth/VolunteerSignUpForm';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '@/app/slices/authSlice';
+
+let mockStore;
 
 describe('auth/SignInForm', () => {
+    beforeEach(() => {
+        mockStore = configureStore({
+            reducer: {
+                auth: authReducer,
+            },
+        });
+    });
     it('should render the form title, fields, sign up button', () => {
-        render(<VolunteerSignUpForm />);
+        render(
+            <Provider store={mockStore}>
+                <VolunteerSignUpForm />
+            </Provider>
+        );
         //title
         expect(screen.getByText('Sign up')).toBeInTheDocument();
         //fields
@@ -18,7 +34,11 @@ describe('auth/SignInForm', () => {
     });
 
     it('should show the errors if fields not filled', async () => {
-        render(<VolunteerSignUpForm />);
+        render(
+            <Provider store={mockStore}>
+                <VolunteerSignUpForm />
+            </Provider>
+        );
         await waitFor(() => {
             fireEvent.click(
                 screen.getByRole('button', {
@@ -32,7 +52,7 @@ describe('auth/SignInForm', () => {
             'First Name is required',
             'Last Name is required',
             'Gender is required',
-            'Age is required',
+            'Age must be more than 12',
             'Phone is required',
             'At least one skill is required',
             'Address is required',
