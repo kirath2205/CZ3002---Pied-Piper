c4a8dfd1646f3ff3d97b12379ba1146b535a35cc
kirath2205 <singhkirath@gmail.com>
2022-03-24 21:51:13 +0800

    Added more tests

--- backend/Authentication/test_views.py
+++ backend/Authentication/test_views.py
@@ -1,3 +1,7 @@
+
+from email import header
+from wsgiref import headers
+from passlib.hash import pbkdf2_sha256
 import json
 from django import setup
 from django.http import response
@@ -6,6 +10,7 @@ from django.test import TestCase,RequestFactory,Client
 from . import views
 from django.urls import reverse
 from backend import error_codes
+from backend.constants_for_tests import *
 
 class AuthenticationViewsTests(TestCase):
 
@@ -13,10 +18,18 @@ class AuthenticationViewsTests(TestCase):
         client = Client()
         self.register_url=reverse('Register')
         self.index_url=reverse('AuthHome')
+        self.refresh_jwt_url=reverse('RefreshJwtToken')
+        self.login_url=reverse('Login')
+        self.verify_jwt_url=reverse('VerifyJwtToken')
+        self.send_otp_url=reverse('send_otp')
+        self.verify_otp_url=reverse('verify_otp')
         UserAccount.objects.create(first_name='Kirath',last_name='Singh',email='singhkirath@gmail.com',
         phone_number='85138731',skills={},age=21,gender='M',address='address')
         OrgAccount.objects.create(name='Org1',email='kirath001@e.ntu.edu.sg',
         phone_number='85138732',address='address')
+        Login.objects.create(email='singhkirath@gmail.com',password=pbkdf2_sha256.hash('Login@1234'),account_type='USER')
+        OTPVerification.objects.create(phone_number='85138731')
+
     def get_response_index(self):
         response=self.client.get(self.index_url)
         self.assertEquals(response.status_code,int(error_codes.api_success()))
@@ -95,3 +108,128 @@ class AuthenticationViewsTests(TestCase):
         response=self.client.post(self.register_url,data,content_type="application/json")
         self.assertEquals(response.status_code,int(error_codes.api_success()))
         self.assertEquals(response.content.decode("utf-8"),"Registered Successfully")
+    
+    def test_verify_jwt_token_local_for_valid_jwt_user_does_not_exist_return_false(self):
+        response=views.verify_jwt_token_local(VALID_JWT_USER_DOES_NOT_EXIST)
+        self.assertEquals(response,False)
+    
+    def test_verify_jwt_token_local_for_expired_jwt_return_false(self):
+        response=views.verify_jwt_token_local(EXPIRED_JWT)
+        self.assertEquals(response,True)
+    
+    def test_verify_jwt_token_local_for_valid_jwt_return_true(self):
+        response=views.verify_jwt_token_local(VALID_JWT)
+        self.assertEquals(response,True)
+    
+    def test_refresh_jwt_token_for_get_request_return_bad_request(self):
+        response=self.client.get(self.refresh_jwt_url)
+        self.assertEquals(response.status_code,int(error_codes.bad_request()))
+        self.assertEquals((response.content).decode("utf-8") ,'404 error')
+    
+    def test_refresh_jwt_token_for_token_not_in_header_return_bad_request(self):
+        response=self.client.post(self.refresh_jwt_url)
+        self.assertEquals(response.status_code,int(error_codes.bad_request()))
+        self.assertEquals((response.content).decode("utf-8")[:21] ,'Deserialisation error')
+        
+    def test_refresh_jwt_token_for_user_does_not_exist_return_invalid_refresh_token(self):
+        header = {
+        'HTTP_AUTHORIZATION':VALID_JWT_USER_DOES_NOT_EXIST}
+        response=self.client.post(self.refresh_jwt_url,**header,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.invalid_refresh_token()))
+        self.assertEquals((response.content).decode("utf-8") ,'Invalid refresh token')
+    
+    def test_refresh_jwt_token_for_valid_refresh_token_return_valid_jwt_token(self):
+        header = {
+        'HTTP_AUTHORIZATION':VALID_JWT}
+        response=self.client.post(self.refresh_jwt_url,**header,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.api_success()))
+        self.assertGreater(len((response.content).decode("utf-8")) ,100)
+    
+    def test_login_for_get_request_return_bad_request(self):
+        response=self.client.get(self.login_url)
+        self.assertEquals(response.status_code,int(error_codes.bad_request()))
+        self.assertEquals((response.content).decode("utf-8") ,'404 error')
+    
+    def test_login_for_no_credentials_return_bad_request(self):
+        response=self.client.post(self.login_url)
+        self.assertEquals(response.status_code,int(error_codes.bad_request()))
+        self.assertEquals((response.content).decode("utf-8")[:21] ,'Deserialisation error')
+    
+    def test_login_for_user_does_not_exist_return_invalid_credentials(self):
+        data={
+            "email" : "singhkirath@gmail"
+        }
+        response=self.client.post(self.login_url,data,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.invalid_credentials()))
+        self.assertEquals((response.content).decode("utf-8") ,'Invalid Credentials')
+    
+    def test_login_for_wrong_password_return_invalid_credentials(self):
+        data={
+            "email" : "singhkirath@gmail.com" , "password" : "Login@123"
+        }
+        response=self.client.post(self.login_url,data,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.invalid_credentials()))
+        self.assertEquals((response.content).decode("utf-8") ,'Invalid Credentials')
+    
+    def test_login_for_valid_credentials_return_login_successful(self):
+        data={
+            "email" : "singhkirath@gmail.com" , "password" : "Login@1234"
+        }
+        response=self.client.post(self.login_url,data,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.login_successful()))
+        self.assertGreater(len((response.content).decode("utf-8")) ,100)
+    
+    def test_verify_token_for_wrong_request_return_bad_request(self):
+        response=self.client.get(self.verify_jwt_url,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.bad_request()))
+        self.assertEquals((response.content).decode("utf-8") ,'404 error')
+    
+    def test_verify_token_for_invalid_data_return_bad_request(self):
+        response=self.client.post(self.verify_jwt_url,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.bad_request()))
+        self.assertEquals((response.content).decode("utf-8")[:21] ,'Deserialisation error')
+    
+    def test_verify_token_for_invalid_token_return_invalid_jwt_token(self):
+        data={"access_token":VALID_JWT_USER_DOES_NOT_EXIST}
+        response=self.client.post(self.verify_jwt_url,data,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.invalid_jwt_token()))
+        self.assertEquals((response.content).decode("utf-8") ,'Invalid jwt token')
+    
+    def test_verify_token_for_valid_token_return_valid_token(self):
+        data={"access_token":EXPIRED_JWT}
+        response=self.client.post(self.verify_jwt_url,data,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.api_success()))
+        self.assertGreater(len((response.content).decode("utf-8")) ,10)
+    
+    def test_send_otp_for_invalid_request_return_bad_request(self):
+        response=self.client.get(self.send_otp_url,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.bad_request()))
+        self.assertEquals((response.content).decode("utf-8") ,'404 error')
+    
+    def test_send_otp_for_invalid_data_return_deserialisation_error(self):
+        response=self.client.post(self.send_otp_url)
+        self.assertEquals(response.status_code,int(error_codes.bad_request()))
+        self.assertEquals((response.content).decode("utf-8")[:21] ,'Deserialisation error')
+    
+    def test_send_otp_for_invalid_phone_number_raise_twilio_exception(self):
+        data={'phone_number':'1'}
+        response=self.client.post(self.send_otp_url,data,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.server_error()))
+        self.assertEquals((response.content).decode("utf-8") ,'Twilio error')
+    
+    def test_send_otp_for_unregistered_phone_number_return_invalid_phone_number(self):
+        data={'phone_number':'90873378'}
+        response=self.client.post(self.send_otp_url,data,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.invalid_phone_number()))
+        self.assertEquals((response.content).decode("utf-8") ,'Invalid phone number')
+
+    def test_send_otp_for_registered_number_return_otp_sent(self):
+        data={'phone_number':'85138731'}
+        response=self.client.post(self.send_otp_url,data,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.otp_sent()))
+        self.assertEquals((response.content).decode("utf-8") ,'OTP sent')
+    
+    def test_verify_otp_for_invalid_request_return_bad_request(self):
+        response=self.client.get(self.verify_otp_url,content_type="application/json")
+        self.assertEquals(response.status_code,int(error_codes.bad_request()))
+        self.assertEquals((response.content).decode("utf-8") ,'404 error')
