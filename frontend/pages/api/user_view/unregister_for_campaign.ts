914044ca0919ee7db850d5298606ecf5594717a8
Sean <56064050+seangoats@users.noreply.github.com>
2022-03-29 12:21:32 +0800

    Add unregister for campaign proxy

---
+++ frontend/pages/api/user_view/unregister_for_campaign.ts
@@ -0,0 +1,47 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { API_URL } from '@/utils/constants/config';
+import axios from 'axios';
+import { getCookieValue } from '@/utils/cookieParser';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        const access = getCookieValue(req, 'access');
+
+        if (!access) {
+            return res.status(217).json({
+                error: 'Something went wrong. Refresh the page and try again.',
+            });
+        }
+
+        const { campaign_id } = req.body;
+
+        try {
+            const apiRes = await axios.post(
+                `${API_URL}/user_view/unregister_for_campaign/`,
+                { campaign_id },
+                {
+                    headers: {
+                        Authorization: access,
+                    },
+                }
+            );
+
+            const data = await apiRes.data;
+
+            if (apiRes.status === 200) {
+                return res.status(200).json({ message: 'Registered for campaign succesfully' });
+            } else {
+                return res.status(400).json({
+                    error: data,
+                });
+            }
+        } catch (err) {
+            return res.status(500).json({
+                error: err,
+            });
+        }
+    } else {
+        res.setHeader('Allow', ['POST']);
+        return res.status(405).json({ error: `Method ${req.method} now allowed` });
+    }
+};
